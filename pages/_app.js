b7192670d09ce30a04f024f51b3bcf43cf8a75cd
Efigenio2025 <joshua.efigenio@icloud.com>
2025-09-11 01:00:39 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // /pages/_app.js
-import '@/styles/globals.css';
+import '../styles/globals.css'; // relative import, no "@/"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
